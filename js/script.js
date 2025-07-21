a1a13e52d73cbe42eb1d6b59519f42abae0b9dfc
kyraella <kyra.ella@web.de>
2025-07-21 15:32:08 +0200

    kommentar test

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ document.getElementById("toggle-button").addEventListener("click", function () {
     const person_list = document.getElementById("person_list");
                 person_list.classList.toggle("show");
             });
+
+
+/* test um github zu testen*/
