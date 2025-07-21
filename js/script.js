d888d735e2b749537a9cec478279c3206ae01244
kyraella <kyra.ella@web.de>
2025-07-21 15:10:49 +0200

    js : toggle-button animation

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle-button").addEventListener("click", function () {
+    const person_list = document.getElementById("person_list");
+                person_list.classList.toggle("show");
+            });
