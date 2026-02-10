4eb76fbb406355bd220902c211347173c0b421c0
lamawa-cmyk <walser.marie@gmail.com>
2026-02-10 11:28:36 +0100

    Mobile: Verzögertes Einblenden

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ if (window.innerWidth <= 768) {
   requestAnimationFrame(() => {
     setTimeout(() => {
       document.body.classList.add("titles-ready");
-    }, 2000);
+    }, 2300);
   });
 }
 
